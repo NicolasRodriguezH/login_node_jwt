197261112fd9bf466daebb02b9d06cc581c274e2
Nicolas <nicolashilarion420@gmail.com>
2022-04-06 16:21:10 -0500

    Added usuary register sistem

--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const { append } = require("express/lib/response");
 const router = express.Router();
 
+const authController = require("../controllers/authController")
+
+// Router para las vistas
 router.get("/", (req, res) => {
     res.render("index");
 })
-
 router.get("/login", (req, res) => {
     res.render("login");
 })
-
 router.get("/register", (req, res) => {
     res.render("register");
 })
 
+// Router para los metodos del controlador
+router.post("/register", authController.register)
 module.exports = router;
